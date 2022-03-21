26ef5c6189510c1a284177a6b63ae7911073df43
Wadabee <bh.jpvw@gmail.com>
2022-03-22 00:34:34 +0900

    implements diff

--- __tests__/formatter.test.ts
+++ __tests__/formatter.test.ts
@@ -1,14 +1,23 @@
 import Formatter from "../src";
 
 describe("formatter", () => {
-  it("1 second ago", () => {
-    const ddf = new Formatter();
-
-    expect(ddf.strictFormat()).toBe("1 second ago");
-    expect(ddf.laxFormat()).toBe("few seconds ago");
+  let ddf: Formatter;
+  beforeAll(() => {
+    ddf = new Formatter(new Date("2022/01/01 00:00:00"));
   });
 
-  it.todo("2 seconds ago");
+  it("1 second ago", () => {
+    const to = new Date("2022/01/01 00:00:01");
+    expect(ddf.strictFormat(to)).toBe("1 second ago");
+    expect(ddf.laxFormat(to)).toBe("few seconds ago");
+  });
+
+  it("2 seconds ago", () => {
+    const to = new Date("2022/01/01 00:00:02");
+    expect(ddf.strictFormat(to)).toBe("2 seconds ago");
+    expect(ddf.laxFormat(to)).toBe("few seconds ago");
+  });
+
   it.todo("1 minute ago");
   it.todo("2 minutes ago");
   it.todo("1 hour ago");
--- src/formatter.ts
+++ src/formatter.ts
@@ -1,19 +1,29 @@
 class Formatter {
-  private from: number;
-  constructor() {
-    this.from = Date.now();
+  private from: Date;
+  constructor(from: Date = new Date(Date.now())) {
+    this.from = from;
   }
 
-  format(): string {
+  // format(to: Date): string {
+  //   return "few seconds ago";
+  // }
+
+  strictFormat(to: Date): string {
+    const diff_ = this.diff(to);
+
+    if (diff_ === 1) {
+      return "1 second ago";
+    } else {
+      return `${diff_} seconds ago`;
+    }
+  }
+
+  laxFormat(to: Date): string {
     return "few seconds ago";
   }
 
-  strictFormat(): string {
-    return "1 second ago";
-  }
-
-  laxFormat(): string {
-    return "few seconds ago";
+  private diff(to: Date): number {
+    return (to.getTime() - this.from.getTime()) / 1000;
   }
 }
 
