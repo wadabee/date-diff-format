682afd3a8f7019cec3c26cdf1786f4c49158f2f8
Wadabee <bh.jpvw@gmail.com>
2022-03-22 00:06:46 +0900

    add todos

--- __tests__/formatter.test.ts
+++ __tests__/formatter.test.ts
@@ -7,4 +7,20 @@ describe("formatter", () => {
     expect(ddf.strictFormat()).toBe("1 second ago");
     expect(ddf.laxFormat()).toBe("few seconds ago");
   });
+
+  it.todo("2 seconds ago");
+  it.todo("1 minute ago");
+  it.todo("2 minutes ago");
+  it.todo("1 hour ago");
+  it.todo("2 hours ago");
+  it.todo("yesterday");
+  it.todo("2 days ago");
+  it.todo("last month");
+  it.todo("2 months ago");
+  it.todo("last year");
+  it.todo("2 years ago");
+
+  it.todo("i18n");
+  it.todo("format customize");
+  it.todo("global setting");
 });
